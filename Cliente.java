997eda229e55cc44793db98829f32e0f239100c2
Karina <acarrascosa@cifpfbmoll.eu>
2021-03-29 18:28:56 +0200

    Clases creadas
    Creado Persona, Empleado, Cliente

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.fp.concesionario;
+
+/**
+ *
+ * @author Karina
+ */
+public class Cliente extends Persona {
+    private Integer reserva;
+    private Integer factura;
+    
+}
---
+++ Empleado.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.fp.concesionario;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Karina
+ */
+public class Empleado extends Persona {
+    private ArrayList<Nomina> listaNominas;
+    private String puestoTrabajo;
+
+    /**
+     * Constructor vacio
+     */
+    public Empleado() {    
+    }
+    
+    /**
+     * Constructor con todos los parametros
+     * @param listaNominas Lista de las nominas
+     * @param puestoTrabajo Puesto de trabajo
+     * @param nombre Nombre del trabajador
+     * @param apellidos Apellidos del trabajador
+     * @param nif NIF del trabajador
+     * @param telefono Telefono del trabajador
+     * @param domicilio Domicilio del trabajador
+     */
+    public Empleado(ArrayList<Nomina> listaNominas, String puestoTrabajo, String nombre, String apellidos, String nif, Integer telefono, String domicilio) {
+        super(nombre, apellidos, nif, telefono, domicilio);
+        this.listaNominas = listaNominas;
+        this.puestoTrabajo = puestoTrabajo;
+    }
+
+    @Override
+    public String toString() {
+        return "{ Nóminas almacenadas: " + listaNominas.size() + " | Puesto de trabajo: " + puestoTrabajo + " }" + super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ArrayList<Nomina> getListaNominas() {
+        return listaNominas;
+    }
+
+    public void setListaNominas(ArrayList<Nomina> listaNominas) {
+        this.listaNominas = listaNominas;
+    }
+
+    public String getPuestoTrabajo() {
+        return puestoTrabajo;
+    }
+
+    public void setPuestoTrabajo(String puestoTrabajo) {
+        this.puestoTrabajo = puestoTrabajo;
+    }
+    
+}
