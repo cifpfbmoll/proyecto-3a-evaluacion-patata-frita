44c7afad2a0f45bcf4f6e5ea1b8e77fa524051a2
JL Cardona <jcardona@cifpfbmoll.eu>
2021-06-02 17:23:59 +0200

    //DevolverTodo en curso

--- Venta.java
+++ Venta.java
@@ -308,4 +308,131 @@ public class Venta {
             }
         }
     }
+    //TODO : Entender y modificar
+    /**
+     * Devuelve los vehiculos filtrados de la base de datos en formato tabla
+     *
+     * @param bastidor
+     * @param tipo
+     * @param kilometraje
+     * @param asientos
+     * @param puertas
+     * @param marca
+     * @param modelo
+     * @param precio
+     * @return
+     */
+    public static Object[][] devolverTodosVehiculosBBDD(String bastidor, String tipo, int kilometraje, int asientos, int puertas,
+                                                        String marca, String modelo, int precio) {
+        boolean where = false;
+        //SQL devuelve Reserva + Nombre cliente + apellidos cliente + tablas relacionadas
+        String consulta = "SELECT vehiculo.*\n" +
+                "FROM test.vehiculo INNER JOIN motor m on vehiculo.motorid = m.id\n" +
+                "LEFT JOIN venta v on vehiculo.ventaid = v.id\n" +
+                "LEFT JOIN cliente c on vehiculo.clientenif = c.nif"; //Cambiar test a concesionario
+        if (bastidor != null && !where) {
+            consulta += " WHERE bastidor like \"" + bastidor + "\"";
+            where = true;
+        }else if(bastidor != null){
+            consulta += " AND bastidor like \"" + bastidor + "\"";
+        }
+        if (tipo != null && !where) {
+            consulta += " WHERE tipo like \"" + tipo + "\"";
+            where = true;
+        }else if(tipo != null){
+            consulta += " AND tipo like \"" + tipo + "\"";
+        }
+        if ( kilometraje > 0 && !where) {
+            consulta += " WHERE kilometraje = \"" + kilometraje + "\"";
+            where = true;
+        }else if(kilometraje > 0){
+            consulta += " AND kilometraje = \"" + kilometraje + "\"";
+        }
+        if (asientos > 0 && !where){
+            consulta += " WHERE asientos = \"" + asientos + "\"";
+            where = true;
+        }else if ( asientos > 0) {
+            consulta += " AND asientos = \"" + asientos + "\"";
+        }
+        if ( puertas > 0 && !where) {
+            consulta += " WHERE puertas = \"" + puertas + "\"";
+            where = true;
+        }else if(puertas > 0){
+            consulta += " AND puertas = \"" + puertas + "\"";
+        }
+        if (precio > 0 && !where){
+            consulta += " WHERE precio = \"" + precio + "\"";
+            where = true;
+        }else if ( precio > 0) {
+            consulta += " AND precio = \"" + precio + "\"";
+        }
+        if (marca != null && !where) {
+            consulta += " WHERE marca like \"" + marca + "\"";
+            where = true;
+        }else if(marca != null){
+            consulta += " AND marca like \"" + marca + "\"";
+        }
+        if (modelo != null && !where) {
+            consulta += " WHERE modelo like \"" + modelo + "\"";
+            where = true;
+        }else if(modelo != null){
+            consulta += " AND modelo like \"" + modelo + "\"";
+        }
+        consulta += " ORDER BY v.bastidor";
+        String[][] objectList = null;
+        try {
+            Utils.prst = Utils.connection.prepareStatement(consulta);
+            Utils.rs = Utils.prst.executeQuery("SELECT COUNT(*) FROM Vehiculo"); // MODIFICAR TABLA EN LAS OTRAS CLASES
+            Utils.rs.next();
+            objectList = new String[Utils.rs.getInt(1)][];
+            int i = 0;
+            Utils.rs = Utils.prst.executeQuery();
+            while (Utils.rs.next()) {
+                //Columnas tiene que ser el numero de columnas que devuelva vuestro sql adaptado
+                //Contar únicamente que columnas son importantes!
+                Integer COLUMNAS = 19;
+                /**
+                 * Bastidor, tipo, clase, kilometraje, autonomia, puertas
+                 * asientos, color, marca, modelo, precio, extras, potencia
+                 * par, tipo, venta id, nombre, apellidos
+                 */
+                //ReservaID, Espacio_res, fecha, clientenif, espacios, horario, nombre,apellidos
+                String[] list = new String[COLUMNAS];
+                list[0] = Utils.rs.getString(1); //Bastidor
+                list[1] = Utils.rs.getString(2); //Vehiculo tipo
+                list[2] = Utils.rs.getString(3); //Estado vehiculo
+                list[3] = Utils.rs.getString(5); //Kilometraje
+                list[4] = Utils.rs.getString(7); //Autonomia
+                list[5] = Utils.rs.getString(8); //Puertas
+                list[6] = Utils.rs.getString(9); //Asientos
+                list[7] = Utils.rs.getString(10); //Color
+                list[8] = Utils.rs.getString(11); //Marca
+                list[9] = Utils.rs.getString(12); //Modelo
+                list[10] = Utils.rs.getString(13); //Precio
+                list[11] = Utils.rs.getString(14); //Extras
+                list[12] = Utils.rs.getString(18); //Potencia
+                list[13] = Utils.rs.getString(19); //Cilindrada
+                list[14] = Utils.rs.getString(20); //Potencia
+                list[15] = Utils.rs.getString(21); //Par
+                list[16] = Utils.rs.getString(22); //Tipo
+                list[17] = Utils.rs.getString(23); //Id venta
+                list[18] = Utils.rs.getString(24); //Horario venta
+                list[19] = Utils.rs.getString(25) + Utils.rs.getString(26); //Nombre + apellidos
+                objectList[i] = list;
+                i++;
+            }
+            return objectList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error mostrando todos los clientes");
+        } finally {
+            try {
+                Utils.cerrarVariables();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar variables");
+            }
+        }
+        return objectList;
+    }
 }
